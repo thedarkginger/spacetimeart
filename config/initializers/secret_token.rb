74cf530368eeeba97fd117c9be54afa83f4175d1
thedarkginger <jp.gooderham@gmail.com>
2014-06-21 01:11:41 -0400

    Created home and about

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spaceart::Application.config.secret_key_base = '06947450b2f9be5954dca114537e643efa3acdebcce6bf411a71504864f44fbd3b18c1a52fd12026c19effc6dc13726255093aec8bce7a3f0d0187259c4304a5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Spaceart::Application.config.session_store :cookie_store, key: '_spaceart_session'
